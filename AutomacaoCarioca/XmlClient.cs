ad144895978339f7ed7cce25d5101aed070758aa
armando monsão <armandomicro@hotmail.com>
2023-03-09 17:23:50 -0300

    Add project files.

---
+++ AutomacaoCarioca/XmlClient.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomacaoCarioca
+{
+    public class XmlClient
+    {
+        [BsonId]
+        public string IdClient { get; set; }
+        public string IdRefLogin => new XmlLogin().IdLogin;
+        public string NameClient { get; set; }
+        public string InscricaoClient { get; set; }
+        public XmlClient()
+        {
+            IdClient = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ AutomacaoCarioca/XmlLogin.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomacaoCarioca
+{
+    public class XmlLogin
+    {
+        [BsonId]
+        public string IdLogin { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public XmlLogin()
+        {
+            IdLogin = Guid.NewGuid().ToString();
+        }
+    }
+}
